8ded11faf705e08bd30c315f282ca447ae34226d
ManOfSteel-329 <142880656+ManOfSteel-329@users.noreply.github.com>
2024-06-11 20:41:27 -0400

    Fix the hashmap concurrency issue

--- src/main/java/com/coderscampus/assignment/A8Solution.java
+++ src/main/java/com/coderscampus/assignment/A8Solution.java
@@ -1,18 +1,18 @@
 package com.coderscampus.assignment;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class A8Solution {
-    private static final int THREAD_COUNT = 1000;
+    private static final int THREAD_COUNT = 100;
     private static final ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
     private static final Assignment8 assignment = new Assignment8();
     // number -> the count of how many times this number has appearedcle
-    private static final Map<Integer, Integer> numberCountMap = new HashMap<>();
+    private static final Map<Integer, Integer> numberCountMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
         CompletableFuture<Void>[] futures = new CompletableFuture[1000];
@@ -27,7 +27,6 @@ public class A8Solution {
         StringBuilder sb = new StringBuilder();
         numberCountMap.forEach((number, count) -> {
             sb.append(number).append("=").append(count).append(", ");
-//            System.out.print(number + "=" + count + ", ");
         });
         sb.deleteCharAt(sb.length() - 2);
         System.out.println(sb);
@@ -40,10 +39,11 @@ public class A8Solution {
         List<Integer> numbersList = assignment.getNumbers();
 
         // 2. update numberCountMap
-        for (Integer n: numbersList) {
-            // numberCountMap.merge(n, 1, Integer::sum);
-            numberCountMap.put(n, numberCountMap.getOrDefault(n,0) + 1);
+        synchronized (numberCountMap) {
+            for (Integer n: numbersList) {
+                // numberCountMap.merge(n, 1, Integer::sum);
+                numberCountMap.put(n, numberCountMap.getOrDefault(n,0) + 1);
+            }
         }
     }
-
 }
